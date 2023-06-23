acfd4b32c0616e6b5f8171a10493286b4986d2d6
kozanakyel <kozanakyel@gmail.com>
2023-06-24 01:40:27 +0300

    add test fucntion for gettransactionbyid method within test files

--- smart_contract/test/sample-test.js
+++ smart_contract/test/sample-test.js
@@ -62,3 +62,47 @@ describe("Transactions", function () {
     });
   });
 });
+
+describe("Transactions", function () {
+  let Transactions;
+  let transactions;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+
+  beforeEach(async function () {
+    Transactions = await ethers.getContractFactory("Transactions");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    transactions = await Transactions.deploy();
+    await transactions.deployed();
+  });
+
+  it("should retrieve transactions by sender", async function () {
+    // Add two transactions from the owner
+    await transactions.connect(owner).addToBlockchain(addr1.address, ethers.utils.parseEther("1.0"), "Test transaction 1", "keyword1", "credit", { value: ethers.utils.parseEther("1.0") });
+    await transactions.connect(owner).addToBlockchain(addr2.address, ethers.utils.parseEther("2.0"), "Test transaction 2", "keyword2", "debit", { value: ethers.utils.parseEther("2.0") });
+
+    // Get transactions sent by the owner
+    const senderTransactions = await transactions.getTransactionsBySender(owner.address);
+
+    // Check that the correct number of transactions was returned
+    expect(senderTransactions.length).to.equal(2);
+
+    // Check the details of the first transaction
+    expect(senderTransactions[0].sender).to.equal(owner.address);
+    expect(senderTransactions[0].receiver).to.equal(addr1.address);
+    expect(senderTransactions[0].amount.toString()).to.equal(ethers.utils.parseEther("1.0").toString());
+    expect(senderTransactions[0].message).to.equal("Test transaction 1");
+    expect(senderTransactions[0].keyword).to.equal("keyword1");
+    expect(senderTransactions[0].transactionType).to.equal("credit");
+
+    // Check the details of the second transaction
+    expect(senderTransactions[1].sender).to.equal(owner.address);
+    expect(senderTransactions[1].receiver).to.equal(addr2.address);
+    expect(senderTransactions[1].amount.toString()).to.equal(ethers.utils.parseEther("2.0").toString());
+    expect(senderTransactions[1].message).to.equal("Test transaction 2");
+    expect(senderTransactions[1].keyword).to.equal("keyword2");
+    expect(senderTransactions[1].transactionType).to.equal("debit");
+  });
+});
